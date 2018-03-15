89311cda866e2422e98afb75e7529abb70967fc8
medrzec <fmynarski@gmail.com>
2018-03-15 15:50:11 +0100

    added preview of 3 dimensional data and custom amount of polynomial features

--- batch_gradient_descent.py
+++ batch_gradient_descent.py
@@ -8,36 +8,43 @@ import matplotlib.pyplot as plt
 
 class GradientDescentManager:
 
-	def __init__(self, X, Y):
+	def __init__(self, X, Y, alpha=0.1, polynomials=5):
 
 		self.raw_data = [X, Y]
 		self.data = [np.array(X), np.array(Y)]
+		self.polynomials = polynomials
+		self.poly = [i for i in range(2, self.polynomials + 2)]
+		self.poly = np.concatenate((self.poly, [1/i for i in range(2, min(4, self.polynomials + 2))]))
 		self.add_features()
 		self.max_values = np.max(self.data[0], 0)[1:]
 		self.min_values = np.min(self.data[0], 0)[1:]
 		self.normalize()
+
 		self.m = len(X)
 		self.N = len(X[0])
-
-		self.alpha = 0.01
+		self.alpha = alpha
 		self.theta = []
 		self.steps = 1
 		self.thetas = np.array([])
 		self.costs = []
 
-	def normalize(self, to_add=False):
+	def normalize(self, to_add=False, idx='False'):
 		if not to_add:
 			for idx, example in enumerate(self.data[0]):
 				self.data[0][idx][1:] = (example[1:] - self.min_values) / (self.max_values - self.min_values)
+		elif idx != 'False':
+			l = int(len(self.min_values)/2)
+			mn = np.array([self.min_values[idx + i*2] for i in range(l)])
+			mx = np.array([self.max_values[idx + i*2] for i in range(l)])
+			return [np.concatenate([[1], (i[1:] - mn) / (mx - mn)]) for i in to_add]
 		else:
 			return [np.concatenate([[1], (i[1:] - self.min_values) / (self.max_values - self.min_values)]) for i in to_add]
 
 	def add_features(self, to_add=False):
 		if not to_add:
-			self.data[0] = [np.concatenate([[1], x, x**2, x**.5]) for x in self.data[0]]
+			self.data[0] = [np.concatenate([[1], x, np.concatenate([x**p for p in self.poly])]) for x in self.data[0]]
 		else:
-			print('to_add',to_add)
-			return np.concatenate([[1], to_add, to_add**2, to_add**.5])
+			return np.concatenate([[1], to_add, np.concatenate([to_add**p for p in self.poly])])
 
 	def compute_cost(self):
 		cost = 0
@@ -90,9 +97,12 @@ class GradientDescentManager:
 			X_to_plot = [np.array([x_min + i*x_step]) for i in range(theta_plot_accuracy+1)]
 			X_modified = self.normalize([self.add_features(i) for i in X_to_plot])
 
-			for j in range(theta_plots):
+			H_to_plot = [self.thetas[0].dot(i) for i in X_modified]
+			plt.plot(X_to_plot, H_to_plot, colors[0], label='initial theta')
+
+			for j in range(1, theta_plots):
 				H_to_plot = [self.thetas[j*theta_step].dot(i) for i in X_modified]
-				plt.plot(X_to_plot, H_to_plot, colors[j], label='theta after ' + str(j*theta_step) + 'iterations')
+				plt.plot(X_to_plot, H_to_plot, colors[j], label='theta after ' + str(j*theta_step) + ' iterations')
 
 			j = theta_plots
 
@@ -113,35 +123,29 @@ class GradientDescentManager:
 			X1 = [i[0] for i in self.raw_data[0]]
 			X2 = [i[1] for i in self.raw_data[0]]
 			Y = self.raw_data[1]
-			ax.scatter(X1, X2, Y)
+			ax.scatter(X1, X2, Y, color='red')
 
-			mn = np.amin(self.raw_data[0], axis=0)
-			mx = np.amax(self.raw_data[0], axis=1)
-			
-			x1_min = mn[0]
-			x1_max = mx[0]
-			x2_min = mn[1]
-			x2_max = mx[1]
+			x1_min = min(X1)
+			x1_max = max(X1)
+			x2_min = min(X2)
+			x2_max = max(X2)
 
-			theta_plot_accuracy = 20
+			theta_plot_accuracy = 40
 
 			X1_to_plot = np.linspace(x1_min, x1_max, num=theta_plot_accuracy)
 			X2_to_plot = np.linspace(x2_min, x2_max, num=theta_plot_accuracy)
 
-			''' not working yet
-			X1_modified = self.normalize([self.add_features(np.array([i])) for i in X1_to_plot])
-			X2_modified = self.normalize([self.add_features(np.array([i])) for i in X2_to_plot])
+			X1_modified = self.normalize([self.add_features(np.array([i])) for i in X1_to_plot], 0)
+			X2_modified = self.normalize([self.add_features(np.array([i])) for i in X2_to_plot], 1)
 
 			X1, X2 = np.meshgrid(X1_to_plot, X2_to_plot)
 
 			H_to_plot = np.zeros((X1.shape))
 			for h in range(len(X1_modified)):
 				for w in range(len(X2_modified)):
-					print(H_to_plot[h][w])
-					print(self.theta.dot([X1_modified[h], X2_modified[w]]))
-					H_to_plot[h][w] = self.theta.dot([X1[h][w], X2[h][w]])
-			'''
+					H_to_plot[h][w] = self.theta.dot(np.concatenate((X1_modified[h], X2_modified[w][1:])))
 
+			ax.contour3D(X1, X2, H_to_plot, 100)
 			ax.set_xlabel('X1 axis')
 			ax.set_ylabel('X2 axis')
 			ax.set_zlabel('Y axis')
@@ -166,7 +170,7 @@ manager.create_theta()
 print('starting cost:', manager.compute_cost())
 print('starting theta:', manager.theta)
 
-manager.run(500)
+manager.run(1000)
 
 print('final cost:', manager.compute_cost())
 print('final theta:', manager.theta)
